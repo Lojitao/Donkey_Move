5fda252bc272da5dba5040c5a7da7abe63e7bb9f
Lojitao <Lojitao@outlook.com>
2023-09-15 17:59:32 +0800

    [update]社會實踐

--- pages/practiced.vue
+++ pages/practiced.vue
@@ -1,5 +1,20 @@
 <template>
-  <p>praticed</p>
+  <main class="min-h-screen">
+    <!-- banner -->
+    <div class="cover">
+      <img class="w-full md:hidden" src="/images/practiced/practiced_bg_mobile.png" alt="">
+      <img class="w-full hidden md:block lg:hidden" src="/images/practiced/practiced_bg_ pad.png" alt="">
+      <img class="w-full hidden lg:block" src="/images/practiced/practiced_bg_pc.png" alt="">
+    </div>
+
+    <!-- 內容 -->
+    <div>
+      <!-- 較紹 -->
+      <section></section>
+      <!-- 列表 -->
+      <section></section>
+    </div>
+  </main>
 </template>
 
 <script setup>
